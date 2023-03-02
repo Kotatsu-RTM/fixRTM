5cc83e2622917634f80cb0281421af98b2b06884
Siro_256 <siro@siro256.dev>
2023-03-02 20:50:20 +0900

    Create functions used from EntityVehicleBase

--- src/main/java/com/anatawa12/fixRtm/rtm/entity/vehicle/EntityVehicleBase.kt
+++ src/main/java/com/anatawa12/fixRtm/rtm/entity/vehicle/EntityVehicleBase.kt
@@ -7,8 +7,13 @@ package com.anatawa12.fixRtm.rtm.entity.vehicle
 import com.anatawa12.fixRtm.addEntityCrashInfoAboutModelSet
 import com.anatawa12.fixRtm.network.NetworkHandler
 import com.anatawa12.fixRtm.network.NotifyUntracked
+import jp.ngt.rtm.entity.train.EntityTrainBase
+import jp.ngt.rtm.entity.train.parts.EntityFloor
 import jp.ngt.rtm.entity.vehicle.EntityVehicleBase
 import net.minecraft.crash.CrashReportCategory
+import net.minecraft.util.math.ChunkPos
+import java.nio.ByteBuffer
+import java.util.UUID
 
 @Suppress("EXTENSION_SHADOWED_BY_MEMBER", "unused")
 fun EntityVehicleBase<*>.onRemovedFromWorld() {
@@ -19,3 +24,38 @@ fun EntityVehicleBase<*>.onRemovedFromWorld() {
 @Suppress("EXTENSION_SHADOWED_BY_MEMBER", "unused")
 fun EntityVehicleBase<*>.addEntityCrashInfo(category: CrashReportCategory) =
     addEntityCrashInfoAboutModelSet(category) { resourceState?.resourceSet?.config }
+
+fun getChunkPosArray(entity: EntityVehicleBase<*>) =
+    mutableSetOf<ChunkPos>().apply {
+        addAll(entity.floors.map { entity.world.getChunk(it.position).pos })
+        if (entity !is EntityTrainBase) return@apply
+        addAll(entity.bogieController.bogies.map { entity.world.getChunk(it.position).pos })
+    }.flatMap { listOf(it.x, it.z) }.toIntArray()
+
+fun intArrayToChunks(array: IntArray) =
+    mutableSetOf<ChunkPos>().apply {
+        for (i in array.indices step 2) {
+            add(ChunkPos(array[i], array[i + 1]))
+        }
+    }
+
+fun floorsUuidToByteArray(vehicleFloors: List<EntityFloor>) =
+    vehicleFloors.map(EntityFloor::getUniqueID).flatMap {
+        ByteBuffer.allocate(16).apply {
+            putLong(it.mostSignificantBits)
+            putLong(it.leastSignificantBits)
+        }.array().toList()
+    }.toByteArray()
+
+fun byteArrayToUuidArray(byteArray: ByteArray): Array<UUID> {
+    if (byteArray.size % 16 != 0) throw IllegalArgumentException("ByteArray size must be multiple of 16")
+
+    val uuids = mutableListOf<UUID>()
+    val buffer = ByteBuffer.wrap(byteArray)
+
+    for (i in byteArray.indices step 16) {
+        uuids.add(UUID(buffer.getLong(i), buffer.getLong(i + 8)))
+    }
+
+    return uuids.toTypedArray()
+}
